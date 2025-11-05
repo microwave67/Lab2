1a15bea9e0c94b17bcf48ccdae063dbab01b1eb4
Jovian Heng <jovianheng.24@ichat.sp.edu.sg>
2025-11-05 09:56:16 +0800

    Updated BMI Return

--- bmi.py
+++ bmi.py
@@ -8,12 +8,15 @@ def calculate_bmi(height, weight):
 
     if calculatedbmi < 18.5 :
         print ("Underweight")
+        return (-1)
     elif 18.5 <= calculatedbmi <= 25.0 :
         print ("Normal")
+        return (0)
     else :
         print ("Overweight")
+        return (1)
 
 
 
 
-calculate_bmi(weight=57, height=1.73)
+print (calculate_bmi(weight=57, height=1.73))
